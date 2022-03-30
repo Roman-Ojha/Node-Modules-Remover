be5a7b4107e166e9ff4dc89e5e0f2e52b8d3388a
Roman Ojha <razzroman98@gmail.com>
2022-03-30 15:46:05 +0545

    finding all files and folder from the given dir

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import os
 import string
 
 
+def findDir(fromDir):
+    for roots, dirs, files in os.walk(fromDir):
+        print(roots, len(dirs), len(files))
+        print(dirs)
+
+
 if __name__ == "__main__":
     folderToDelete = "node_modules"
     fromDir = string
@@ -9,9 +15,14 @@ if __name__ == "__main__":
     excludedDirs = []
     fromDir = input(
         f"Enter Directory path from where you want to delete {folderToDelete} folder\n")
-    excludedDirNo = input(
-        f"Enter how many Directory you wan to exclude to search to delete {folderToDelete} folder: ")
-    excludedDirNo = int(excludedDirNo)
-    for i in range(0, excludedDirNo, 1):
-        excludedDir = input(f"Enter {i+1}th Directory you want to exclude\n")
-        excludedDirs.append(excludedDir)
+    # excludedDirNo = input(
+    #     f"Enter how many Directory you wan to exclude to search to delete {folderToDelete} folder: ")
+    # excludedDirNo = int(excludedDirNo)
+    # for i in range(0, excludedDirNo, 1):
+    #     excludedDir = input(f"Enter {i+1}th Directory you want to exclude\n")
+    #     excludedDirs.append(excludedDir)
+    isDirExist = os.path.exists(fromDir)
+    if(isDirExist):
+        findDir(fromDir)
+    else:
+        print("not exist")
